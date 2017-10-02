cadc05d720b35536407efea2418da97aa0210793
YeisonGomez <yeigomez97@hotmail.com>
2017-10-02 01:24:22 -0500

    update table and restart game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,20 @@ export class AppComponent {
 
 	public boxs;
   public turno;
+  public users;
+  public letters;
 
   constructor(private boxService: BoxService){
+    this.letters = this.boxService.letters;
+    this.boxs = this.boxService.getTable(10);
   	this.getBoxsAll();
   }
 
   private getBoxsAll(){
   	this.boxService.getAll()
   	.subscribe(data => {
-  		this.boxs = data.json().tablero;
+      this.users = data.json().tablero;
+  		this.boxs = this.boxService.addingUsersToTable(this.users, this.boxs);
       this.turno = data.json().turno;
       console.log(this.boxs);
   	});
@@ -29,6 +34,36 @@ export class AppComponent {
   private restartGame(){
     this.boxService.startGame()
     .subscribe(data => {
+      this.boxs = this.boxService.getTable(10);
+      this.getBoxsAll();
     });
+  }
+
+  private updateTable(){
+    this.boxService.updateTable()
+    .subscribe(data => {
+      this.users = data.json().tablero;
+      this.updateBoxsSecuencie(data.json().tablero);
+      this.turno = data.json().turno;
+    });
+  }
+
+  private updateBoxsSecuencie(users){
+    for (let i = 0; i < this.boxs.length; ++i) {
+      for (let l = 0; l < this.boxs[i].length; ++l) {
+        let user_exist = -1;
+        for (let j = 0; j < users.length; ++j) {
+          if(users[j].casilla && users[j].casilla == this.boxs[i][l].casilla){
+            user_exist = j;
+            break;
+          }
+        }
+        if(user_exist == -1){
+          this.boxs[i][l] = { casilla: this.boxs[i].casilla };
+        } else {
+          this.boxs[i][l] = users[user_exist];
+        }
+      }
+    }
   }
 }
--- src/services/box.service.ts
+++ src/services/box.service.ts
@@ -4,17 +4,42 @@ import { ApiService } from '../services/api.service';
 @Injectable()
 export class BoxService {
 	
+	private code_private = 321;
+	public letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
+
 	constructor(public api: ApiService) {}
 
 	getAll(){
-    	return this.api.POST('/box/table/get-table-private', `key=${321}`);
+    	return this.api.POST('/box/table/get-table-private', `key=${this.code_private}`);
   	}
 
   	updateTable(){
-    	return this.api.POST('/box/update-table', `key=${321}`);
+    	return this.api.POST('/box/update-table', `key=${this.code_private}`);
   	}
 
   	startGame(){
-    	return this.api.POST('/box/start-game', `key=${321}`);
+    	return this.api.POST('/box/start-game', `key=${this.code_private}`);
+  	}
+
+  	getTable(size){
+  		let table = [];
+  		for (let i = 0; i < size; ++i) {
+  			table[i] = [];
+  			for (let j = 1; j < size + 1; ++j) {
+  				table[i].push({ casilla: this.letters[i] + j });
+  			}
+  		}
+  		return table;
+  	}
+
+  	addingUsersToTable(users, table){
+  		for (let i = 0; i < users.length; ++i) {
+  			if(users[i].casilla){
+  				let c_num = parseInt(users[i].casilla.substring(1, users[i].casilla.length));
+  				let index = this.letters.indexOf(users[i].casilla.substring(0, 1));
+  				table[index][(c_num - 1)] = users[i];
+  			}
+  		}
+  		return table;
   	}
 }
